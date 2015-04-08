c077554bd45a461b5c786df550485c86ac5f4437
AntiHaus <contact@antihaus.com>
2015-04-07 20:44:41 -0400

    Knuth Shuffle an array in Swift.
    Create a deck of cards, and give it a shuffle.

--- Knuth_CardShuffle/main.swift
+++ Knuth_CardShuffle/main.swift
@@ -1,12 +1,24 @@
-//
-//  main.swift
-//  Knuth_CardShuffle
-//
-//  Created by AntiHaus on 07/04/2015.
-//  Copyright (c) 2015 AntiHaus. All rights reserved.
-//
-
 import Foundation
 
-println("Hello, World!")
+var rank = ["A","2","3","4","5","6","7","8","9","10","J","K"]
+var suit = ["♠", "♥","♦","♣"]
 
+var deck = [String]()
+for t in suit {
+    for r in rank {
+        deck.append("\(r)\(t)")
+    }
+    
+}
+
+func shuffleDeck(var deckShuffled:[String]) -> [String] {
+    
+    for var i = deckShuffled.count-1; i > 0; i-- {
+        let j = Int(arc4random_uniform(UInt32(i+1)))
+        (deckShuffled[i], deckShuffled[j]) = (deckShuffled[j], deckShuffled[i])
+    }
+    return deckShuffled
+}
+
+println("Unshuffled: \(deck) \n")
+println("Knuth Shuffle: \(shuffleDeck(deck)) \n")
